b0e1168d1a76c1b0095eda937e2c728c28b18231
mathunada07 <mathunada07@gmail.com>
2020-09-07 16:41:50 -0400

    testing

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var ring: UIImageView!
+    @IBOutlet weak var ScoutingButton: RoundButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIView.animate(withDuration: 2, delay: 0, options: [.repeat], animations: ({
+            self.ring.transform = CGAffineTransform(rotationAngle: CGFloat.pi/8.5*6)
+        }))
         // Do any additional setup after loading the view.
     }
 
-
 }
-
